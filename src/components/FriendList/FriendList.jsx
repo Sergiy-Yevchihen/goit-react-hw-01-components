ca6f62216420325bfdec6e71b25f27afaee2c1c8
Serhii Yevchishen <magodza06@gmail.com>
2023-05-16 21:49:42 +0300

    add frends

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import './friendlist.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
